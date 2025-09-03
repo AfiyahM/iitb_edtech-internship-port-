a5466fce863a0617abdec72c2dca43639cc02e75
AfiyahM <afu2974@gmail.com>
2025-09-03 12:22:13 +0530

    dashboard layout error resolved

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -3,7 +3,7 @@
 import type React from "react"
 
 import { useState, useRef, useEffect } from "react"
-import { DashboardLayout } from "@/components/dashboard-layout"
+import  DashboardLayout  from "@/components/dashboard-layout"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
